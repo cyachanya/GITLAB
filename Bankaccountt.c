2b169fa354bafb37be53aa74cd2dd025d139eef9
cyachanya <cyz.chanyacya@msn.com>
2015-04-22 21:30:02 +0700

    Add files to git

---
+++ Bankaccountt.c
@@ -0,0 +1,256 @@
+/************************************************************************************************
+ *                                                                                              *
+ * Project Name : Bank Account.c                                                                *
+ *                                                                                              *
+ * Detail : This program allows the user to enter account number and starting balance,          *
+ *          If account number is equal to -1, it will print all of transaction and end program  *
+ *          Then ask the user about amount. If amount is more than 0, it will be deposit.       *
+ *          If amount is less than 0, it will be withdrawal. If amount is equal to 0, it will   *
+ *          print the invoice and this user is finished.                                        *
+ *                                                                                              *
+ * Create By : Benjawan  Junpum                                                                 *
+ *             Student ID :                                                                     *
+ *                                                                                              *
+ * Date : 16-Aug-2010                                                                           *
+ *                                                                                              *
+ ************************************************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX 100
+
+void getAccount(char account[])
+{
+    char input[32];
+    int check;
+    int i;
+    do
+        {
+        check=1;
+        printf("Enter your account number (8 digit, not start with 0,1):");
+        fgets(input,32,stdin);
+        sscanf(input,"%s",account);
+        for(i=0;i<strlen(account);i++)
+            {
+            if(isdigit(account[i])==0)
+                {
+                check=0;
+                }
+            }
+        if(account[0]=='0'||account[0]=='1'||strlen(account)==0||strlen(account)!=8)
+            {
+            check=0;
+            }
+        if(check==0)
+            {
+            printf("Error\n");
+            }
+        }
+    while(check==0&&!(account[0]=='-'&&account[1]=='1'&&account[2]=='\0'));
+
+}
+
+double getBalance()
+{
+    char input[32];
+    double balance;
+    int check;
+    int i;
+    do
+        {
+        check=1;
+        printf("Enter your balance :");
+        fgets(input,32,stdin);
+        for(i=0;i<strlen(input)-1;i++)
+            {
+            if(isdigit(input[i])==0)
+                {
+                check=0;
+                }
+            }
+        sscanf(input,"%lf",&balance);
+        if(strlen(input)-1==0)
+            {
+            check=0;
+            }
+        if(check==0)
+            {
+            printf("Error\n");
+            }
+        }
+    while(check==0);
+    return balance;
+}
+
+double getTransaction(int count,double balance,double deposit[],double withdraw[],double bonus[],double fee[])
+{
+    char input[32];
+    double trans;
+    int check;
+    int i;
+    deposit[count]=0;
+    withdraw[count]=0;
+    bonus[count]=0;
+    fee[count]=0;
+
+
+    do
+        {
+    do
+        {
+        check=1;
+        printf("Enter your transaction (+ deposit, - withdraw) :");
+        fgets(input,32,stdin);
+        for(i=0;i<strlen(input)-1;i++)
+            {
+            if(isdigit(input[i])==0&&input[0]!='-')
+                {
+                check=0;
+                }
+            }
+        sscanf(input,"%lf",&trans);
+        if(strlen(input)-1==0)
+            {
+            check=0;
+            }
+
+        if(check==0)
+            {
+            printf("Error\n");
+            }
+        }
+    while(check==0);
+    if(trans>0)
+    {
+        deposit[count]=trans;
+        printf("Balance         : %.2lf\n",balance);
+        printf("Deposit         : %.2lf\n",deposit[count]);
+        printf("Current balance : %.2lf\n",balance+deposit[count]);
+        if(deposit[count]>=500000)
+        {
+            bonus[count]=100;
+            printf("You deposit more than 500000 baht\n");
+            printf("You get 100 baht bonus\n");
+        }
+    }
+    if(trans<0)
+    {
+        if(balance<-1*trans)
+        {
+            printf("You don't have enough money\n");
+            check=0;
+        }
+        else
+        {
+            withdraw[count]=-1*trans;
+            printf("Balance         : %.2lf\n",balance);
+            printf("Withdraw        : %.2lf\n",withdraw[count]);
+            printf("Current balance : %.2lf\n",balance-withdraw[count]);
+            if(withdraw[count]>=100000)
+            {
+                fee[count]=10;
+                printf("You withdraw more than 100000 baht\n");
+                printf("You have to pay fee 10 baht\n");
+            }
+        }
+    }
+    }
+    while(check==0);
+    return trans;
+}
+
+void showSlip(char account[],int count,double startBalance,double balance,double deposit[],double withdraw[],double bonus[],double totalBonus,double fee[],double totalFee)
+{
+    int i;
+    printf("Bank account     : %s\n",account);
+    printf("Start balance    : %.2lf\n",startBalance);
+    for(i=0;i<count;i++)
+    {
+        if(deposit[i]!=0)
+        {
+            printf("Deposit          : %.2lf\n",deposit[i]);
+        }
+        if(withdraw[i]!=0)
+        {
+            printf("Withdraw         : %.2lf\n",withdraw[i]);
+        }
+    }
+    printf("Total bonus      : %.2lf\n",totalBonus);
+    printf("Total fee        : %.2lf\n",totalFee);
+    printf("Current balance  : %.2lf\n",balance);
+}
+
+void endDay(int customer,double allDeposit,double allWithdraw,double allBonus,double allFee)
+{
+    printf("Total number of customer : %d\n",customer);
+    printf("Total of all deposit     : %.2lf\n",allDeposit);
+    printf("Total of all withdraw    : %.2lf\n",allWithdraw);
+    printf("Total bonuses            : %.2lf\n",allBonus);
+    printf("Total fees               : %.2lf\n",allFee);
+}
+
+int main()
+{
+    char account[32];
+    int count=0;
+    int customer=0;
+    double trans=-1;
+    double startBalance=0;
+    double balance=0;
+    double deposit[MAX]={0};
+    double withdraw[MAX]={0};
+    double bonus[MAX]={0};
+    double fee[MAX]={0};
+    double totalBonus=0;
+    double totalFee=0;
+    double allDeposit=0;
+    double allBonus=0;
+    double allWithdraw=0;
+    double allFee=0;
+
+    getAccount(account);
+    while(strcmp(account,"-1"))
+        {
+        trans=-1;
+        startBalance=getBalance();
+        balance=startBalance;
+        while(trans!=0)
+            {
+            trans=getTransaction(count,balance,deposit,withdraw,bonus,fee);
+            if(trans!=0)
+                {
+                /*each customer*/
+                balance=balance+deposit[count]-withdraw[count]+bonus[count]-fee[count];
+                totalBonus=totalBonus+bonus[count];
+                totalFee=totalFee+fee[count];
+
+                /*overall*/
+                allDeposit=allDeposit+deposit[count];
+                allWithdraw=allWithdraw+withdraw[count];
+                allBonus=allBonus+bonus[count];
+                allFee=allFee+fee[count];
+
+                count++;
+                }
+            else
+                {
+
+                showSlip(account,count,startBalance,balance,deposit,withdraw,bonus,totalBonus,fee,totalFee);
+                /*finish customer*/
+                customer++;
+
+                /*reset for new customer*/
+                count=0;
+                startBalance=0;
+                balance=0;
+                totalBonus=0;
+                totalFee=0;
+                }
+            }
+
+
+        getAccount(account);
+        }
+    endDay(customer,allDeposit,allWithdraw,allBonus,allFee);
+}
---
+++ main.c
@@ -0,0 +1,247 @@
+/*
+  main.c
+  BankAccount
+  This program make for doing the transaction in the bank.
+  This program could calculate bonus and fee when you doing the transaction.
+  This program will print the translip for telling the user what money that they have in Bank Account and what transaction that they did.
+  And at the end of the day it will print the summary of every transaction and every Bank Account.
+*/
+
+/*
+  Created by CHANYA NINLAPHAT ID:55070503460
+  Department of Computer Engineering Class:D
+  On 10/18/55 BE.
+ Copyright (c) 2555 CHANYA NINLAPHAT All rights reserved.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+/* this function print end of the day */
+void printEndOfTheDay(int TotalAllAccount,double TotalAllDeposit,double TotalAllWithdrawal,double TotalAllBonus,double TotalAllFee) 
+{
+    printf("All Account %d\n",TotalAllAccount);
+    printf("Total All Deposit  %.2lf Baht\n",TotalAllDeposit);
+    printf("Total AllWithdrawal %.2lf Baht\n",TotalAllWithdrawal);
+    printf("Total Bonus %.2lf Baht\n",TotalAllBonus);
+    printf("Total Fee %.lf Baht\n",TotalAllFee);
+}
+
+/* this function print end transaction */
+void printEndTransaction(char AccountNum[],double TotalBeforeBalance,double TotalDeposit,double TotalWithdrawal,double TotalBonus,double TotalFee,double Balance)
+{
+    printf("Account Number %s\n",AccountNum);
+    printf("Before Balance %.2lf Bath\n",TotalBeforeBalance);
+    printf("Total Deposit  %.2lf Baht\n",TotalDeposit);
+    printf("Total Withdrawal %.2lf Baht\n",TotalWithdrawal);
+    printf("Total Bonus %.2lf Baht\n",TotalBonus);
+    printf("Total Fee %.lf Baht\n",TotalFee);
+    printf("Balance %.2lf Baht\n",Balance);
+}
+
+/* this function print transction */
+void printTransactions(char AccountNum[],double BeforeBalance,double transactionMoney,double Bonus,double Fee,double Balance)
+{
+    printf("Account Number %s\n",AccountNum);
+    printf("Before Balance %.2lf Bath\n",BeforeBalance);
+    if(transactionMoney > 0)
+    {
+        printf("Deposit  %.2lf Baht\n",transactionMoney);
+        if(Bonus > 0)
+        {
+            printf("Bonus %.2lf Baht\n",Bonus);
+        }
+    }
+    if(transactionMoney < 0)
+    {
+        printf("Withdrawal %.2lf Baht\n",transactionMoney);
+        if(Fee < 0)
+        {
+            printf("Fee %.lf Baht\n",Fee);
+        }
+    }
+    printf("Balance %.2lf Baht\n",Balance);
+    
+}
+
+/* this function calculate */
+void calculate(char AccountNum[],double* pBalance,double* pBeforeBalance,double* pTotalBeforeBalance,double* pTotalDeposit,double* pTotalWithdrawal,double* pTotalFee,double* pTotalBonus,double* pTotalAllDeposit,double* pTotalAllWithdrawal,double* pTotalAllFee,double* pTotalAllBonus)
+{
+    char input[32];
+    double transactionMoney;
+    double Bonus = 0;
+    double Fee = 0;
+    *pTotalBeforeBalance = *pBalance;
+    do
+    {
+        *pBeforeBalance = *pBalance;
+        Bonus = 0;
+        Fee = 0;
+        printf("How much do you want to deposit or withdrawal ? ");
+        fgets(input,32,stdin);
+        sscanf(input,"%lf",&transactionMoney);
+        if(transactionMoney < 0)
+        {
+            if(transactionMoney <= -100000)
+            {
+                Fee = -13;
+                if(*pBalance >= -(transactionMoney+Fee))
+                {
+                    *pBalance += transactionMoney+Fee;
+                    *pTotalWithdrawal += transactionMoney;
+                    *pTotalFee += Fee;
+                    *pTotalAllWithdrawal += transactionMoney;
+                    *pTotalAllFee += Fee;
+                    printTransactions(AccountNum,*pBeforeBalance,transactionMoney,Bonus,Fee,*pBalance);
+                }
+                else
+                {
+                    printf("Balance money is not enough to withdrawal\n");
+                }
+            }
+            else
+            {
+                if(*pBalance >= -transactionMoney)
+                {
+                    *pBalance += transactionMoney;
+                    *pTotalWithdrawal += transactionMoney;
+                    *pTotalAllWithdrawal += transactionMoney;
+                    printTransactions(AccountNum,*pBeforeBalance,transactionMoney,Bonus,Fee,*pBalance);
+                }
+                else
+                {
+                    printf("Balance money is not enough to withdrawal\n");
+                }
+            }
+        }
+        else if(transactionMoney > 0)
+        {
+            if(transactionMoney >= 500000)
+            {
+                Bonus = 100;
+                *pBalance += transactionMoney+Bonus;
+                *pTotalDeposit += transactionMoney;
+                *pTotalBonus += Bonus;
+                *pTotalAllDeposit += transactionMoney;
+                *pTotalAllBonus += Bonus;
+            }
+            else
+            {
+                *pBalance += transactionMoney;
+                *pTotalDeposit += transactionMoney;
+                *pTotalAllDeposit += transactionMoney;
+            }
+            printTransactions(AccountNum,*pBeforeBalance,transactionMoney,Bonus,Fee,*pBalance);
+        }
+    }
+    while(transactionMoney != 0);
+}
+/* this function start balance */
+void startBalance(double* pBalance)
+{
+    char input[32];
+    int check = 0;
+    while(check == 0)
+    {
+        printf("What start balance? ");
+        fgets(input,32,stdin);
+        sscanf(input,"%lf",pBalance);
+        if(*pBalance >= 0)
+        {
+            check = 1;
+        }
+        else
+        {
+            printf("Balance money must not less than 0 Baht\n");
+        }
+    }
+}
+
+/*  this function get account number */
+int getAccount(char AccountNum[])
+{
+    char input[32];
+    int i,a=0;
+    printf("Please enter your account number ");
+    fgets(input,32,stdin);
+    sscanf(input,"%s",AccountNum);
+	
+    for(i=0;i<8;i++)
+    {
+        if(AccountNum[i]=='-'&&AccountNum[i+1]=='1'&&strlen(AccountNum)==2) //
+            break;
+        if((AccountNum[i]>='0'&&AccountNum[i]<='9')==0) 
+            a=1;
+        if(a==1)
+        {
+            printf("Account number must not be a letters\n");
+            return 0;
+        }
+    }
+    if(strlen(AccountNum) == 8) 
+    {
+        
+        if(AccountNum[0] == '0' || AccountNum[0] == '1') 
+        {
+            printf("Account number must not start with 0 or 1\n");
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    else if(strlen(AccountNum) == 2) 
+    {
+        if(strcmp(AccountNum,"-1") == 0) 
+        {
+            return 1;
+        }
+        else
+        {
+            printf("Account number must have 8 numbers\n");
+            return 0;
+        }
+    }
+    else
+    {
+        printf("Account number must have 8 numbers\n");
+        return 0;
+    }
+}
+
+int main()
+{
+    char accountNum[10];
+    double balance = 0;
+    double beforeBalance = 0;
+    double totalBeforeBalance = 0;
+    double totalDeposit = 0;
+    double totalWithdrawal = 0;
+    double totalFee = 0;
+    double totalBonus = 0;
+    int totalAllAccount = 0;
+    double totalAllDeposit = 0;
+    double totalAllWithdrawal = 0;
+    double totalAllFee = 0;
+    double totalAllBonus = 0;
+    int i,a=0;
+    
+    do
+    {
+        while(getAccount(accountNum) == 0); /* use function getAccount */
+        if(strcmp(accountNum,"-1") == 0) /* end of the day */
+			{ } 
+        else
+        {
+            totalAllAccount = totalAllAccount+1;
+            startBalance(&balance);
+            printf("Balance = %lf\n",balance);
+            calculate(accountNum,&balance,&beforeBalance,&totalBeforeBalance,&totalDeposit,&totalWithdrawal,&totalFee,&totalBonus,&totalAllDeposit,&totalAllWithdrawal,&totalAllFee,&totalAllBonus);
+            printEndTransaction(accountNum,totalBeforeBalance,totalDeposit,totalWithdrawal,totalBonus,totalFee,balance); 
+        }
+    }
+    while(!strcmp(accountNum,"-1") == 0);
+    printEndOfTheDay(totalAllAccount, totalAllDeposit, totalAllWithdrawal, totalAllBonus, totalAllFee);
+
+    }
